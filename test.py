6c571f256ed70d4eed06f5e2264ce1d49518919b
Fomichev Vladislav Alexandrovich <34138348+FoVlaX@users.noreply.github.com>
2019-10-24 20:40:02 +0300

    Add files via upload
    opana

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
 from PIL import Image, ImageDraw, ImageFont
-symbols = [' ','`','.','-','+','=','*','i','u','v','o','g','w','&','%','@','#']
-im = Image.open("test2.jpg")
+import sys
 
-width = int(im.size[0] / 2)
-height = int(im.size[1] / 2)
-newim = im.resize((width,height),Image.ANTIALIAS).convert('L')
+symbols = [' ','`','.','-','+','=','*','i','u','v','o','g','w','&','%','@','#']
+
+#symbols = ['贝','非','给','得','也','用','热','赛','伊']
+s = sys.argv[1]
+im = Image.open(s)
+
+width = int(im.size[0] / 8)
+height = int(im.size[1] / 8)
+newim = im.resize((width,height),Image.ANTIALIAS)
+vpix = newim.load()
+newim = newim.convert('L')
+
+
+
 pix = newim.load()
 
-mn = pix[0,0]
-mx = pix[0,0]
+f = open(s.split('.')[0]+'.txt','w')
 
-for i in range(width):
-    for j in range(height):
-        mn = min(mn,pix[i,j])
-        mx = max(mx,pix[i,j])
+saveim = Image.new('RGB',(15*width,15*height),(0,0,0))
 
-print(mn,' ',mx)
-
-f = open('test.txt','w')
-
-saveim = Image.new('L',(15*width,15*height),255)
-
-font = ImageFont.truetype('arial.ttf',15)
+font = ImageFont.truetype('ariblk.ttf',15)
 
 draw = ImageDraw.Draw(saveim)
 
@@ -32,11 +32,20 @@ for i in range(height):
         f.write(symbols[ind]+symbols[ind])
     f.write('\n')
 
-
+if (len(sys.argv) > 2 and sys.argv[2] == "1"):
+    for i in range(height):
+        for j in range(width):
+            ind = len(symbols)-1 - int(pix[j,i]/256*len(symbols))
+            draw.rectangle([j*15,i*15,j*15+15,i*15+15],fill = (int(vpix[j,i][0]/6),int(vpix[j,i][1]/6),int(vpix[j,i][2]/6)))
+            
 for i in range(height):
     for j in range(width):
         ind = len(symbols)-1 - int(pix[j,i]/256*len(symbols))
-        draw.text((j*15,i*15),symbols[ind],font = font)
-saveim.save("result.jpg")
+        draw.text((j*15,i*15),symbols[ind],font = font,fill = vpix[j,i],stroke_fill = vpix[j,i])
+
+
+
+saveim.save(s.split('.')[0]+'result.jpg')
+print("complite")
 f.close()
 
