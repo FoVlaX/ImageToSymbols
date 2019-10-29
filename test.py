a0e43f9547ef7c52d178770c9cfe203033d83d67
Fomichev Vladislav Alexandrovich <34138348+FoVlaX@users.noreply.github.com>
2019-10-29 13:46:14 +0300

    Add files via upload
    fixed size symbol and image scale

--- test.py
+++ test.py
@@ -5,10 +5,19 @@ symbols = [' ','`','.','-','+','=','*','i','u','v','o','g','w','&','%','@','#']
 
 #symbols = ['贝','非','给','得','也','用','热','赛','伊']
 s = sys.argv[1]
+sybolsize = int(sys.argv[2])
+imagesize = float(sys.argv[3])
+
 im = Image.open(s)
 
-width = int(im.size[0] / 8)
-height = int(im.size[1] / 8)
+
+
+
+kkk = imagesize/float(im.size[1])/sybolsize;
+
+
+width = int(im.size[0] * kkk)
+height = int(im.size[1] * kkk)
 newim = im.resize((width,height),Image.ANTIALIAS)
 vpix = newim.load()
 newim = newim.convert('L')
@@ -19,9 +28,9 @@ pix = newim.load()
 
 f = open(s.split('.')[0]+'.txt','w')
 
-saveim = Image.new('RGB',(15*width,15*height),(0,0,0))
+saveim = Image.new('RGB',(sybolsize*width,sybolsize*height),(255,255,255))
 
-font = ImageFont.truetype('ariblk.ttf',15)
+font = ImageFont.truetype('ariblk.ttf',sybolsize)
 
 draw = ImageDraw.Draw(saveim)
 
@@ -32,16 +41,16 @@ for i in range(height):
         f.write(symbols[ind]+symbols[ind])
     f.write('\n')
 
-if (len(sys.argv) > 2 and sys.argv[2] == "1"):
+if (len(sys.argv) > 4 and sys.argv[4] == "1"):
     for i in range(height):
         for j in range(width):
             ind = len(symbols)-1 - int(pix[j,i]/256*len(symbols))
-            draw.rectangle([j*15,i*15,j*15+15,i*15+15],fill = (int(vpix[j,i][0]/6),int(vpix[j,i][1]/6),int(vpix[j,i][2]/6)))
+            draw.rectangle([j*sybolsize,i*sybolsize,j*sybolsize+sybolsize,i*sybolsize+sybolsize],fill = (int(vpix[j,i][0]/6),int(vpix[j,i][1]/6),int(vpix[j,i][2]/6)))
             
 for i in range(height):
     for j in range(width):
         ind = len(symbols)-1 - int(pix[j,i]/256*len(symbols))
-        draw.text((j*15,i*15),symbols[ind],font = font,fill = vpix[j,i],stroke_fill = vpix[j,i])
+        draw.text((j*sybolsize,i*sybolsize),symbols[ind],font = font,fill = vpix[j,i],stroke_fill = vpix[j,i])
 
 
 
